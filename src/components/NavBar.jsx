fbe4bf5afcb9e4a27be0755218a1b9070477a248
AshmitSherigar <sherigarashmit@gmail.com>
2025-01-07 14:37:02 +0530

    navbar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NavBar.css"
+import { IoIosArrowDown } from "react-icons/io"
+import { use } from 'react'
 function NavBar() {
+  const [isDown, setisDown] = useState(false)
+  const toggleArrow = () => {
+    setisDown((prev) => !prev)
+  }
   return (
     <div className='navbar-box'>
-        <h1>Biggie</h1>
-        <h5>Location</h5>
-        <h5>Search for restaurant,cuisine or a dish</h5>
+      <h1>Biggie</h1>
+      <h5>Location</h5>
+      <h5>Search for restaurant,cuisine or a dish</h5>
+      <div className="accounts" onClick={toggleArrow}>
+        <div className="circle"></div>
+        <h4>UserName</h4>
+        <IoIosArrowDown className={isDown ? "rotateUp" : ""} id='icon' />
+      </div>
+      <div className="nav-options">
+        <h2>Profile</h2>
+        <h2>Reviews</h2>
+        <h2>Settings</h2>
+        <h2>Log out</h2>
+      </div>
     </div>
   )
 }
